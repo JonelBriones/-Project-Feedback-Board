ed916cf6f1134a619c9b5e17848d617c287353c3
JonelBriones <jonel.c.briones@gmail.com>
2024-12-04 02:59:04 -0800

    added connectDB()

--- app/suggestion/[id]/page.tsx
+++ app/suggestion/[id]/page.tsx
@@ -9,14 +9,15 @@ import { Feedback as FeedbackT } from "@/types";
 import Feedback from "@/models/Feedback";
 import { convertToSerializableObject } from "@/utils/convertToObject";
 import { Session } from "next-auth";
+import connectDB from "@/config/database";
 
 const page = async ({ params }: any) => {
   const { id } = await params;
-
+  await connectDB();
   if (id.length != 24) {
     return <NoAccess url={"/"} id={id} text={"Suggestion not found."} />;
   }
-  const result = await Feedback.findById(id);
+  const result = await Feedback.findById(id).lean();
 
   if (!result) {
     return <NoAccess url={"/"} id={id} text={"Suggestion not found."} />;
@@ -25,11 +26,11 @@ const page = async ({ params }: any) => {
   // const suggestionById: FeedbackT | null = convertToSerializableObject(result);
   const suggestionById = JSON.parse(JSON.stringify(result));
 
-  // const session: Session | null = await auth();
+  const session: Session | null = await auth();
 
   return (
     <div className="max-w-[540px] w-[100vw] flex flex-col gap-4 h-screen overflow-auto mt-10 md:mt-0">
-      {/* <div className="flex place-items-center justify-between">
+      <div className="flex place-items-center justify-between">
         <GoBack />
         {session?.user?.id == suggestionById?.owner && (
           <LinkButton
@@ -38,7 +39,7 @@ const page = async ({ params }: any) => {
             url={`/suggestion/${id}/edit`}
           />
         )}
-      </div> */}
+      </div>
       <CommentContainer feedback={suggestionById} suggestionID={id} />
     </div>
   );
